c9317d239d0134572691fdc8efb1bdc1c8249551
ZwelNyiNyarHtut <zwelnyinyinyar@gmail.com>
2024-09-04 15:28:18 +0630

    responsive nav bar but need to add dropdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'pale-green': '#f8fff0',
+        'sage-green':'#87986a',
+        'forest-green' :'#1E3F20',
+      },
+
+      screens:{
+        'max-md': { 'max': '1024px' },
+      },
+    },
   },
   plugins: [],
 }

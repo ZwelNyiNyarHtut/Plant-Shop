9e4166b733917d1a923cbebba1777360a3d5bca4
ZwelNyiNyarHtut <zwelnyinyinyar@gmail.com>
2024-09-04 17:35:21 +0630

    nav bar with drop down

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
         'pale-green': '#f8fff0',
         'sage-green':'#87986a',
         'forest-green' :'#1E3F20',
+        'light-green' :'#f8fff0',
       },
 
       screens:{
         'max-md': { 'max': '1024px' },
+        'max-sm': {'max': '479px'},
       },
     },
   },
